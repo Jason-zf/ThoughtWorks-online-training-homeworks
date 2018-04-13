06d413dd82122084d40553ae010a500bfc81f44d
Jason <baofeng_zheng@163.com>
2018-04-13 19:51:58 +0800

    add get_count_test to fizzbuzzgametest and add get_count method to class fizzbuzzgame

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -6,13 +6,18 @@ import java.util.List;
 
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
+    private Integer count;
 
     public void start(int count) {
-
+        this.count = count;
     }
 
     public List<String> getResults() {
         return results;
+    }
+
+    public Integer getCount() {
+        return count;
     }
 }
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,8 +1,24 @@
 package com.thoughtworks;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzGameTest {
+    private FizzBuzzGame fizzBuzzGame;
+
+    @Before
+    public void setFizzBuzzGame() {
+        fizzBuzzGame = new FizzBuzzGame();
+    }
+
+    @Test
+    public void should_return_student_count_when_getCount() {
+        assertEquals(fizzBuzzGame.getCount(), null);
+    }
+
     @Test
     public void should_return_Fizz_when_have_3() {
 
