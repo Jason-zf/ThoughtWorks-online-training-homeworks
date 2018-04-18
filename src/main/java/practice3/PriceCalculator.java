91d0c187e9a34f3ff82da5d5860b32d5024c504c
Jason <baofeng_zheng@163.com>
2018-04-18 13:55:27 +0800

    complete practice3

---
+++ src/main/java/practice3/PriceCalculator.java
@@ -0,0 +1,45 @@
+package practice3;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class PriceCalculator {
+    private List<OrderLineItem> orderLineItemList;
+    private List<BigDecimal> discounts;
+    private BigDecimal tax;
+
+    public PriceCalculator(List<OrderLineItem> orderLineItemList, List<BigDecimal> discounts, BigDecimal tax) {
+        this.orderLineItemList = orderLineItemList;
+        this.discounts = discounts;
+        this.tax = tax;
+    }
+
+    public BigDecimal calculate() {
+        BigDecimal subTotal = calculateSubTotal();
+        subTotal = subtractDiscount(subTotal);
+        BigDecimal grandTotal = addTax(subTotal);
+        return grandTotal;
+    }
+
+    public BigDecimal calculateSubTotal() {
+        BigDecimal subTotal = new BigDecimal(0);
+        for (OrderLineItem lineItem : orderLineItemList) {
+            subTotal = subTotal.add(lineItem.getPrice());
+        }
+        return subTotal;
+    }
+
+    public BigDecimal subtractDiscount(BigDecimal subTotal) {
+        for (BigDecimal discount : discounts) {
+            subTotal = subTotal.subtract(discount);
+        }
+        return subTotal;
+    }
+
+    public BigDecimal addTax(BigDecimal subTotal) {
+        BigDecimal tax = subTotal.multiply(this.tax);
+        BigDecimal grandTotal = subTotal.add(tax);
+        return grandTotal;
+    }
+
+}
