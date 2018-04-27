68f48620023eafccb6cba0b807905080afc60b3f
Jason <baofeng_zheng@163.com>
2018-04-27 15:18:59 +0800

    modify spring_boot_application_test

--- src/test/java/tw/springboot/SpringbootApplicationTests.java
+++ src/test/java/tw/springboot/SpringbootApplicationTests.java
@@ -3,6 +3,7 @@ package tw.springboot;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,6 +11,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import tw.springboot.controller.EmployeesController;
+import tw.springboot.core.Employee;
+
+import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -20,54 +24,56 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(classes = MockServletContext.class)
 public class SpringbootApplicationTests {
     private MockMvc mvc;
+    RequestBuilder requestBuilder;
 
     @Before
     public void setUp() {
         mvc = MockMvcBuilders.standaloneSetup(new EmployeesController()).build();
+        requestBuilder = null;
     }
 
     @Test
-    public void testEmployeesController() throws Exception {
-        // 1、get查一下user列表，应该为空
-        RequestBuilder requestBuilder = null;
+    public void should_return_all_employees_json_when_get_all() throws Exception {
         requestBuilder = get("/employees/");
         mvc.perform(requestBuilder)
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("[]")));
+                .andExpect(content().string(equalTo("[{\"id\":0,\"name\":\"小明\",\"age\":20,\"gender\":\"男\"},{\"id\":1,\"name\":\"小红\",\"age\":19,\"gender\":\"女\"},{\"id\":2,\"name\":\"小智\",\"age\":15,\"gender\":\"男\"},{\"id\":3,\"name\":\"小刚\",\"age\":16,\"gender\":\"男\"},{\"id\":4,\"name\":\"小霞\",\"age\":15,\"gender\":\"女\"}]")));
+    }
 
-        // 2、post提交一个user
+    @Test
+    public void should_return_id_0_employee_when_get_0() throws Exception {
+        requestBuilder = get("/employees/0");
+        mvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":0,\"name\":\"小明\",\"age\":20,\"gender\":\"男\"}")));
+    }
+
+    @Test
+    public void should_return_add_success_when_post_employee_json() throws Exception {
         requestBuilder = post("/employees/")
-                .param("id", "1")
+                .param("id", "5")
                 .param("name", "小明")
                 .param("age", "20")
                 .param("gender", "男");
         mvc.perform(requestBuilder)
                 .andExpect(content().string(equalTo("add success")));
+    }
 
-        // 3、get获取user列表，应该有刚才插入的数据
-        requestBuilder = get("/employees/");
+    @Test
+    public void should_return_delete_success_when_delete_employee() throws Exception {
+        requestBuilder = delete("/employees/0");
         mvc.perform(requestBuilder)
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"小明\",\"age\":20,\"gender\":\"男\"}]")));
+                .andExpect(content().string(equalTo("delete success")));
+    }
 
-        // 4、put修改id为1的user
-        requestBuilder = put("/employees/1")
+    @Test
+    public void should_return_update_success_when_update_one_employee() throws Exception {
+        requestBuilder = put("/employees/0")
                 .param("name", "小红")
                 .param("age", "20")
                 .param("gender", "女");
         mvc.perform(requestBuilder)
                 .andExpect(content().string(equalTo("update success")));
-
-        // 6、del删除id为1的user
-        requestBuilder = delete("/employees/1");
-        mvc.perform(requestBuilder)
-                .andExpect(content().string(equalTo("delete success")));
-
-        // 7、get查一下user列表，应该为空
-        requestBuilder = get("/employees/");
-        mvc.perform(requestBuilder)
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("[]")));
     }
 
 }
