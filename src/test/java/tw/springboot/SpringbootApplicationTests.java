cf63418bb1ea0cf1995d8c06f7516458e817ff9c
Jason <baofeng_zheng@163.com>
2018-04-27 21:44:40 +0800

    modify test requestbody 400 error

--- src/test/java/tw/springboot/SpringbootApplicationTests.java
+++ src/test/java/tw/springboot/SpringbootApplicationTests.java
@@ -1,10 +1,11 @@
 package tw.springboot;
 
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -12,8 +13,6 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import tw.springboot.controller.EmployeesController;
 import tw.springboot.core.Employee;
-
-import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -27,9 +26,13 @@ public class SpringbootApplicationTests {
     RequestBuilder requestBuilder;
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         mvc = MockMvcBuilders.standaloneSetup(new EmployeesController()).build();
-        requestBuilder = null;
+        requestBuilder = post("/employees/")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\":0,\"name\":\"小明\",\"age\":20,\"gender\":\"男\"}");
+        mvc.perform(requestBuilder);
     }
 
     @Test
@@ -37,7 +40,7 @@ public class SpringbootApplicationTests {
         requestBuilder = get("/employees/");
         mvc.perform(requestBuilder)
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("[{\"id\":0,\"name\":\"小明\",\"age\":20,\"gender\":\"男\"},{\"id\":1,\"name\":\"小红\",\"age\":19,\"gender\":\"女\"},{\"id\":2,\"name\":\"小智\",\"age\":15,\"gender\":\"男\"},{\"id\":3,\"name\":\"小刚\",\"age\":16,\"gender\":\"男\"},{\"id\":4,\"name\":\"小霞\",\"age\":15,\"gender\":\"女\"}]")));
+                .andExpect(content().string(equalTo("[{\"id\":0,\"name\":\"小明\",\"age\":20,\"gender\":\"男\"}]")));
     }
 
     @Test
@@ -51,10 +54,9 @@ public class SpringbootApplicationTests {
     @Test
     public void should_return_add_success_when_post_employee_json() throws Exception {
         requestBuilder = post("/employees/")
-                .param("id", "5")
-                .param("name", "小明")
-                .param("age", "20")
-                .param("gender", "男");
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\":0,\"name\":\"小明\",\"age\":20,\"gender\":\"男\"}");
         mvc.perform(requestBuilder)
                 .andExpect(content().string(equalTo("add success")));
     }
@@ -69,9 +71,9 @@ public class SpringbootApplicationTests {
     @Test
     public void should_return_update_success_when_update_one_employee() throws Exception {
         requestBuilder = put("/employees/0")
-                .param("name", "小红")
-                .param("age", "20")
-                .param("gender", "女");
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\":0,\"name\":\"小红\",\"age\":20,\"gender\":\"女\"}");
         mvc.perform(requestBuilder)
                 .andExpect(content().string(equalTo("update success")));
         requestBuilder = get("/employees/0");
