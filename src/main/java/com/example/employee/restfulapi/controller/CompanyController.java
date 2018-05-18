7d141347ed86c60c23095651fcec509e31871c6b
Jason <baofeng_zheng@163.com>
2018-05-18 21:52:39 +0800

    complete company findall and findbyid request

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -1,5 +1,30 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Company;
+import com.example.employee.restfulapi.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/companies")
 public class CompanyController {
-    //在此处完成Company API
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<Company> findAll() {
+        return companyRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Company findById(@PathVariable Long id) {
+        return companyRepository.findOne(id);
+    }
+
+
 }
