c22a2d2fc5e70f220e9dca44411fd6b2e3d5a0d2
Jason <baofeng_zheng@163.com>
2018-05-18 20:26:14 +0800

    add findcompanynamebyname method to employeerepository

--- src/test/java/com/example/employee/EmployeeJPATest.java
+++ src/test/java/com/example/employee/EmployeeJPATest.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -60,7 +62,7 @@ public class EmployeeJPATest {
     public void should_return_employee_list_when_input_page_request() throws Exception {
         //4.实现对Employee的分页查询，每页两条数据，一共三页数。
         //注意：PageRequest的构造方法已经弃用了代替的是PageRequest.of,并且最后一个参数代表按照table中的哪一个字段排序
-        Page<Employee> EmployeePage = null;
+        Page<Employee> EmployeePage = employeeRepository.findAll(PageRequest.of(3, 2, Sort.Direction.DESC, "salary"));
         assertThat(EmployeePage.getTotalPages()).isEqualTo(3);
     }
 
@@ -68,7 +70,7 @@ public class EmployeeJPATest {
     public void should_return_company_name_when_input_employee_name() throws Exception {
         //5.查找xiaohong的所在的公司的公司名称
         String expectedCompanyName = "alibaba";
-        String actualCompanyName = null;
+        String actualCompanyName = employeeRepository.findCompanyNameByName("xiaohong");
         assertThat(actualCompanyName).isEqualTo(expectedCompanyName);
     }
 
