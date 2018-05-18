601b89d9fb97cef3c34af532b0783fb138c87888
Jason <baofeng_zheng@163.com>
2018-05-18 21:56:51 +0800

    complete employee findall and findbyid request

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -1,5 +1,29 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.repository.EmployeeRepository;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/employees")
 public class EmployeeController {
-    //在此处完成Employee API
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Employee findById(@PathVariable Long id) {
+        return employeeRepository.findOne(id);
+    }
 }
