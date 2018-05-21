e03ad596a9e24e7132b7b5c850ccbccf8deb19cf
Jason <baofeng_zheng@163.com>
2018-05-21 14:49:20 +0800

    complete employee eurd request

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -2,12 +2,8 @@ package com.example.employee.restfulapi.controller;
 
 import com.example.employee.restfulapi.entity.Employee;
 import com.example.employee.restfulapi.repository.EmployeeRepository;
-import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,4 +22,36 @@ public class EmployeeController {
     public Employee findById(@PathVariable Long id) {
         return employeeRepository.findOne(id);
     }
+
+    @RequestMapping(value = "/male", method = RequestMethod.GET)
+    public List<Employee> findAllMale() {
+        return employeeRepository.findAllByGender("male");
+    }
+
+    @RequestMapping(value = "/female", method = RequestMethod.GET)
+    public List<Employee> findAllFemale() {
+        return employeeRepository.findAllByGender("female");
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public void addOne(@RequestBody Employee employee) {
+        employeeRepository.saveAndFlush(employee);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public void updateOne(@PathVariable Long id, @RequestBody Employee employee) {
+        Employee employee1 = employeeRepository.findOne(id);
+        employee1.setName(employee.getName());
+        employee1.setAge(employee.getAge());
+        employee1.setGender(employee.getGender());
+        employee1.setCompanyId(employee.getCompanyId());
+        employee1.setSalary(employee.getSalary());
+        employeeRepository.saveAndFlush(employee1);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteOne(@PathVariable Long id) {
+        employeeRepository.delete(id);
+    }
+
 }
