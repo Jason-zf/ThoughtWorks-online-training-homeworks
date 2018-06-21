f5600ca17279a13b7bd14aaf2d1c59052a099f8e
baofeng <baofeng_zheng@163.com>
2018-06-21 15:49:19 +0800

    complete Datetimes 14

---
+++ src/main/com/tw/DateTimes14.java
@@ -0,0 +1,12 @@
+package com.tw;
+
+import java.time.LocalTime;
+import java.util.Calendar;
+
+public class DateTimes14 {
+    public static void main(String[] args) {
+        LocalTime localTime = LocalTime.now();
+
+        System.out.println("Local time:" + localTime);
+    }
+}
