f415874399bf21ecada83e9c3c8067bec5518e8f
baofeng <baofeng_zheng@163.com>
2018-06-25 08:51:51 +0800

    complete Datetimes 20

---
+++ src/main/com/tw/DateTimes20.java
@@ -0,0 +1,11 @@
+package com.tw;
+
+import java.time.Instant;
+
+public class DateTimes20 {
+    public static void main(String[] args) {
+        Instant timestamp = Instant.now();
+
+        System.out.println("Current timestamp:" + timestamp);
+    }
+}
