c2c803b2a0d1cd2248e94f8ca80771c5dba3fda5
baofeng <baofeng_zheng@163.com>
2018-06-26 22:41:40 +0800

    complete Numbers 13

---
+++ src/main/com/tw/Numbers13.java
@@ -0,0 +1,27 @@
+package com.tw;
+
+import java.util.Scanner;
+
+public class Numbers13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a number : ");
+        int num = sc.nextInt();
+        if (isPronicNum(num)) {
+            System.out.println("Pronic Number.");
+        } else {
+            System.out.println("Not a Pronic Number.");
+        }
+    }
+
+    private static boolean isPronicNum(int num) {
+        boolean flag = false;
+        for (int i = 0; i < num; ++i) {
+            if (i * (i + 1) == num) {
+                flag = true;
+                break;
+            }
+        }
+        return flag;
+    }
+}
