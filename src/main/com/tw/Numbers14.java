0708ce6a85599452f200cc3d0d12ceac51064725
baofeng <baofeng_zheng@163.com>
2018-06-26 22:46:33 +0800

    complete Numbers 14

---
+++ src/main/com/tw/Numbers14.java
@@ -0,0 +1,23 @@
+package com.tw;
+
+import java.util.Scanner;
+
+public class Numbers14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a number : ");
+        int num = sc.nextInt();
+        if (isAutomorphicNum(num)) {
+            System.out.println("Automorphic Number.");
+        } else {
+            System.out.println("Not an Automorphic Number.");
+        }
+    }
+
+    private static boolean isAutomorphicNum(int num) {
+        int sqrt = num * num;
+        String originalStr = Integer.toString(num);
+        String newStr = Integer.toString(sqrt);
+        return newStr.endsWith(originalStr);
+    }
+}
