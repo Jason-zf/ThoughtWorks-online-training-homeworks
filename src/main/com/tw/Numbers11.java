9cd080523ccb0930814b37403ddf6ff1fa556fff
baofeng <baofeng_zheng@163.com>
2018-06-26 22:25:14 +0800

    complete Numbers 11

---
+++ src/main/com/tw/Numbers11.java
@@ -0,0 +1,30 @@
+package com.tw;
+
+import java.util.Scanner;
+
+public class Numbers11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input a number : ");
+        int num = sc.nextInt();
+        if (isDisariumNum(num)) {
+            System.out.println("Disarium Number.");
+        } else {
+            System.out.println("Not a Disarium Number.");
+        }
+    }
+
+    private static boolean isDisariumNum(int num) {
+        int len = Integer.toString(num).length();
+        int sum = 0;
+        int copy = num;
+
+        while (copy > 0) {
+            int buff = copy % 10;
+            sum += Math.pow(buff, len);
+            len--;
+            copy /= 10;
+        }
+        return sum == num;
+    }
+}
