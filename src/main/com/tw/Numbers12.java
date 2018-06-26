6f92ae8d03f328e2658a12cae8a0c7174edcea29
baofeng <baofeng_zheng@163.com>
2018-06-26 22:34:52 +0800

    complete Numbers 12

---
+++ src/main/com/tw/Numbers12.java
@@ -0,0 +1,27 @@
+package com.tw;
+
+import java.util.Scanner;
+
+public class Numbers12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input a number : ");
+        int num = sc.nextInt();
+        if (isHarshadNum(num)) {
+            System.out.println(num + " is a Harshad Number.");
+        } else {
+            System.out.println(num + " is not a Harshad Number.");
+        }
+    }
+
+    private static boolean isHarshadNum(int num) {
+        int sum = 0;
+        int copy = num;
+        while (copy > 0) {
+            sum += copy % 10;
+            copy /= 10;
+        }
+        return num % sum == 0;
+    }
+}
